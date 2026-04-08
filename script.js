77c7dea5e0f2d2b5dc2148963925379ac562cd3e
Chathura Rathanayake <chathurarathnayake443@gmail.com>
2026-04-08 20:16:36 +0530

    added mist again

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+//
+//     const navBtn = document.querySelector('#ham-icon');
+//     const navList = document.querySelector('.nav-list');
+//
+//     navBtn.addEventListener('click', function() {
+//     navList.classList.toggle('active');
+// })
 
+document.addEventListener("DOMContentLoaded", function () {
     const navBtn = document.querySelector('#ham-icon');
     const navList = document.querySelector('.nav-list');
 
-    navBtn.addEventListener('click', function() {
-    navList.classList.toggle('active');
-})
+    if (navBtn && navList) {
+        navBtn.addEventListener('click', function () {
+            navList.classList.toggle('active');
+        });
+    }
+});
