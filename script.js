16675b72dc2554e4164f89053317b4f96f7eb5cd
Chathura Rathanayake <chathurarathnayake443@gmail.com>
2026-04-08 19:05:39 +0530

    created mobile list bar

---
+++ script.js
@@ -0,0 +1,7 @@
+
+    const navBtn = document.querySelector('#ham-icon');
+    const navList = document.querySelector('.nav-list');
+
+    navBtn.addEventListener('click', function() {
+    navList.classList.toggle('active');
+})
